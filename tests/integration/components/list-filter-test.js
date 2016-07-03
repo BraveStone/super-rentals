abfb32466a1df14c5fc4e66f39c8ae7fc25b8871
BraveStone <shiyong0416@gmail.com>
2016-07-03 14:00:18 +0800

    building a complex component

---
+++ tests/integration/components/list-filter-test.js
@@ -0,0 +1,47 @@
+import { moduleForComponent, test } from 'ember-qunit';
+import hbs from 'htmlbars-inline-precompile';
+
+import Ember from 'ember';
+import wait from 'ember-test-helpers/wait';
+
+moduleForComponent('list-filter', 'Integration | Component | list filter', {
+  integration: true
+});
+
+const ITEMS = [{city: 'San Francisco'}, {city: 'Portland'}, {city: 'Seattle'}];
+const FILTERED_ITEMS = [{city: 'San Francisco'}];
+
+test('should initially load all listings', function(assert) {
+  // Set any properties with this.set('myProperty', 'value');
+  // Handle any actions with this.on('myAction', function(val) { ... });
+
+  // we want our actions to return promises, since they are potentially fetching data asynchronously
+  this.on('filterByCity', (val) => {
+    if (val === '') {
+      return Ember.RSVP.resolve(ITEMS);
+    } else {
+      return Ember.RSVP.resolve(FILTERED_ITEMS);
+    }
+  });
+
+  // with an integration test, you can set up and use your component in the same way your application
+  // will use it.
+  this.render(hbs`
+    {{#list-filter filter=(action 'filterByCity') as |results|}}
+      <ul>
+      {{#each results as |item|}}
+        <li class="city">
+          {{item.city}}
+        </li>
+      {{/each}}
+      </ul>
+    {{/list-filter}}
+  `);
+
+  // the wait function will return a promise that will wait for all promises
+  // and xhr requests to resolve before running the contents of the then block.
+  return wait().then(() => {
+    assert.equal(this.$('.city').length, 3);
+    assert.equal(this.$('.city').first().text().trim(), 'San Francisco');
+  });
+});
